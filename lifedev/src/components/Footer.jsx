ac07fe9253620dcd33194a5d5b2d04b7a78fa6fe
Leandro Silva <leandroedaniela2019@gmail.com>
2023-10-26 22:41:26 -0300

    Aula 2610 - Estrutura de CSS e Fonts

--- lifedev/src/components/Footer.jsx
+++ lifedev/src/components/Footer.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import style from './Footer.module.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className={style.footer}>
+      <h3>Log da vida de desenvolvedor</h3>
+      <p>Life dev &copy; 2023</p>
+    </footer>
   )
 }
 
--- lifedev/src/components/Navbar.jsx
+++ lifedev/src/components/Navbar.jsx
@@ -5,21 +5,34 @@ import styles from "./Navbar.module.css"
 const Navbar = () => {
   return (
     <>
+    <nav className={styles.navbar}>
+
       <NavLink to="/">
-        <span>LifeDev</span>
+        Life <span>Dev</span>
       </NavLink>
-      <ul>
+      <ul className={styles.links_list}>
         <li>
-          <NavLink to="/">
+          <NavLink to="/" className={({ isActive }) => (isActive? styles.active : null)}>
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink to="/about">
+          <NavLink to="/login" className={({ isActive }) => (isActive? styles.active : null)}>
+            Login
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/register" className={({ isActive }) => (isActive? styles.active : null)}>
+            Cadastrar
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" className={({ isActive }) => (isActive? styles.active : null)}>
             About
           </NavLink>
         </li>
       </ul>
+    </nav>
     </>
   )
 }
