cb2872a5cf05433261273519dea760499e785f82
Leandro <leandroedaniela2019@gmail.com>
2023-11-06 22:39:26 -0300

    Aula 0611 - Montagem da Estrutura CSS

--- lifedev/src/pages/Register/Register.jsx
+++ lifedev/src/pages/Register/Register.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const [displayname, setDisplayname] = useState('')
+const [emailUsuario, setEmailUsuario] = useState('')
+const [senhaUsuario, setSenhaUsuario] = useState('')
+const [confirmacaoUsuario, setonfirmacaoUsuario] = useState('')
+const [erro, setErro] = useState('')
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div>
+      <h1>Cadastro de LifeDeveloper</h1>
+      <form>
+        <label>
+          <span>Nome:</span>
+          <input type="text" name="displayname" placeholder='Digite seu nome de usuário' required />
+        </label>
+        <label>
+          <span>Email:</span>
+          <input type="email" name="emailUsuario" placeholder='Entre com seu email' required />
+        </label>
+        <label>
+          <span>Senha:</span>
+          <input type="password" name="senhaUsuario" placeholder='Entre com uma senha segura' required />
+        </label>
+        <label>
+          <span>Confirmação:</span>
+          <input type="password" name="confirmacaoUsuario" placeholder='Confirme sua senha' required />
+        </label>
+        <button className='btn'>Cadastrar</button>
+      </form>
+    </div>
+
   )
 }
 
